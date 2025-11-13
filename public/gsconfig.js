1fd59aced74d111f144e6a276e9c54462934b2f0
Joaquin Conrado Bregant <joabresper@gmail.com>
2025-11-13 20:26:00 -0300

    archivo de config de geoserver cambiado

--- public/gsconfig.js
+++ public/gsconfig.js
@@ -1,12 +1,6 @@
 const gs = {
-  url: 'http://localhost:8080/geoserver',
-  workspace: 'tpigis',
-  wms: function() {	
-    return `${this.url}/${this.workspace}/wms`;
-  },
-  wfs: function() {
-    return `${this.url}/${this.workspace}/wfs`;
-  }
+  url: 'http://localhost:8080/geoserver/ows',
+  workspace: 'tpigis'
 };
 
 export default gs;
