6b56f492d8678f1c631377384b307cb9f6e04049
Camilo Aguirre <camiloo12.c1@gmail.com>
2025-12-02 11:32:25 -0300

    Cambio de titulo y capa base

--- public/maps/layers.js
+++ public/maps/layers.js
@@ -3,13 +3,7 @@ import { Z_INDEX_BASE } from "../const.js";
 
 export const base_layer = new ol.layer.Tile({
   title: 'Base Layer',
-  source: new ol.source.TileWMS({
-    url: 'https://wms.ign.gob.ar/geoserver/ows',
-    params: {
-      'LAYERS': 'ign:provincia',
-      'TILED': true
-    }
-  })
+  source: new ol.source.OSM()
 });
 
 export const capas = {
